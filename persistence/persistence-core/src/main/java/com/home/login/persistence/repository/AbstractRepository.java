2ff125257842b8fde5f07e7a047c1778cf4aceb0
nadya05danielyan <nadya.ds@gmail.com>
2015-05-28 23:00:52 +0400

    created web-admin

--- persistence/persistence-core/src/main/java/com/home/login/persistence/repository/AbstractRepository.java
+++ persistence/persistence-core/src/main/java/com/home/login/persistence/repository/AbstractRepository.java
@@ -1,12 +1,12 @@
 package com.home.login.persistence.repository;
 
 import com.home.login.service.common.AbstractEntityModel;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created: Nadya Dainelyan
  * DATE:    5/25/15
  * TIME:    7:11 PM
  */
-public interface AbstractRepository<T extends AbstractEntityModel> extends CrudRepository<T, Long> {
+public interface AbstractRepository<T extends AbstractEntityModel> extends JpaRepository<T, Long> {
 }
